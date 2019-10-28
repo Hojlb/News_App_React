50a43fafa1ac5c75d6e89208aa39d1e0f46ac312
Max Polonsky <mchp010nsky@gmail.com>
2019-10-29 00:39:07 +0300

    feat: build project

---
+++ build/precache-manifest.ca0f99a44b6811a5152d127cb207d730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e861b0af73c05b22f5509920779927",
+    "url": "/News_App_React/index.html"
+  },
+  {
+    "revision": "cdf4cc104b3872e32198",
+    "url": "/News_App_React/static/css/main.8a5f2d6d.chunk.css"
+  },
+  {
+    "revision": "d37cd5372db76e785f7e",
+    "url": "/News_App_React/static/js/2.95a1ff93.chunk.js"
+  },
+  {
+    "revision": "cdf4cc104b3872e32198",
+    "url": "/News_App_React/static/js/main.5a5d145f.chunk.js"
+  },
+  {
+    "revision": "5a8d839b6f7b2d3f8ded",
+    "url": "/News_App_React/static/js/runtime~main.2f7fa38b.js"
+  }
+]);
